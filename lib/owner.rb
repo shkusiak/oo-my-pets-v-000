aab4657814b059e3e82fe0002a62f4292d867283
Shea Kusiak <shkusiak@gmail.com>
2018-04-15 00:28:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:fishes].count} fish, #{} dog(s), and #{} cat(s)."
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
 
   def self.reset_all
